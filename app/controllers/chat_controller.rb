0d22260a28e715c12d72c0f6b2e94c16aadb1bad
Morgan Grubb <morgan@wishpond.com>
2012-08-28 23:26:24 -0700

    Stupid

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -11,7 +11,7 @@ class ChatController < ApplicationController
 
     messages.collect! do |message|
       payload = message.attributes
-      payload[:user] = user.attributes
+      payload[:user] = message.chat_user.attributes
       payload[:created_at_formatted] = Time.now.in_time_zone("Pacific Time (US & Canada)").to_s(:short)
       payload
     end

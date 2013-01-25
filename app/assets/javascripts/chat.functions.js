d53282311bce6d75852a4158fbc6da24683cbdbe
Morgan Grubb <miggles@gmail.com>
2013-01-24 21:15:40 -0800

    Improved url linkage and newline handling

--- app/assets/javascripts/chat.functions.js
+++ app/assets/javascripts/chat.functions.js
@@ -278,13 +278,14 @@ function scrollToTheTop(force) {
   hideNewMessageNotification();
 }
 
+var urlRegex = /\b(?:(?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\((?:[^\s()<>]+|(\(?:[^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
+
 function replaceURLWithHTMLLinks(text) {
-     var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-     return text.replace(exp,"<a href='$1' target='_blank'>$1</a>");
+  return text.replace(urlRegex, "<a href='$&' target='_blank'>$&</a>");
 }
 
 function replaceNewLinesWithLineBreaks(text) {
-	return text.replace(/[\r\n]+/, "<br />");
+	return text.replace(/[\r\n]+/g, "<br />");
 }
 
 function setPlaceholder(names) {

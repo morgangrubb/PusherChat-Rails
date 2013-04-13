3317dbd6e63c79782b6a71b793888ef09fbfd5c1
Morgan Grubb <miggles@gmail.com>
2013-04-12 21:39:35 -0700

    Favicon fixed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     message.gsub(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i, "<a href='\\1' target='_blank'>\\1</a>".html_safe).html_safe
   end
 
+  def asset_url(asset)
+    "#{request.protocol}#{request.host_with_port}#{asset_path(asset)}"
+  end
+
 end

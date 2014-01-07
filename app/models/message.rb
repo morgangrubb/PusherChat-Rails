3af380099356f226eeabe708f76aee05627eeb7b
Morgan Grubb <morgan@wishpond.com>
2014-01-06 21:57:56 -0800

    Truncate because of Ryan

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   belongs_to :chat_user, foreign_key: "user_id"
 
+  before_validate :truncate_message
+
   after_create :touch_last_active
 
   def timestamp
@@ -9,5 +11,11 @@ class Message < ActiveRecord::Base
 
   def touch_last_active
     chat_user.touch(:last_active_at)
+  end
+
+  def truncate_message
+    if message.length > 1000
+      self.message = message[0..999] + " (message truncated by system because Ryan MacDonald is a dick)"
+    end
   end
 end
